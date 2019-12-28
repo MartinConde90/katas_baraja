2ea71b97dfb1471e86a8d084b2017f1a145a3c6b
= <=>
2019-12-28 18:30:53 +0100

    Posible test repair

--- baraja.py
+++ baraja.py
@@ -14,16 +14,39 @@ def baraja():
 def elige_carta(i, longitud):
     return random.randint(0, longitud-1)
 
-
-
 def mezclar(b):
     for i in range(len(b)):
         #Elegimos carta de intercambio al azar
         al_azar = elige_carta(i, len(b))
 
         #Cambiamos la carta por orden(i) por la carta elegida al azar
-
         aux = b[i]
         b[i] = b[al_azar]
         b[al_azar] = aux
+
     return(b)
+
+class Baraja():
+    naipes = []
+    __palos = ['o', 'c', 'e', 'b']
+    __cartas = ['A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R']
+
+    def __init__(self):
+        self.naipes = []
+        for palo in _palos:
+            for carta in _cartas:
+                self.naipes.append(carta + palo)
+
+    def elige_carta(self, i):
+        return random.randint(0, len(self.naipes)-1)
+
+
+    def mezclar(self):
+        for i in range(len(self.naipes)):
+            #Elegimos carta de intercambio al azar
+            al_azar = self.elige_carta(i, len(self.naipes))
+
+            #Cambiamos la carta por orden(i) por la carta elegida al azar
+            aux = self.naipes[i]
+            self.naipes[i] = self.naipes[al_azar]
+            self.naipes[al_azar] = aux
